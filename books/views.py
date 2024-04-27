574f2f95c2c17330aa719ffa57ec71c0b5b54b85
Eugene Protsenko <guitaaarpro@gmail.com>
2024-04-27 14:41:39 +0200

    Add permissions

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from books.models import Book
 from books.serializers import BookListSerializer
@@ -9,4 +9,10 @@ from books.serializers import BookListSerializer
 class BookListViewSet(viewsets.ModelViewSet):
     serializer_class = BookListSerializer
     queryset = Book.objects.all()
-    # permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ["list"]:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
